d505b3edd911f4508d7cea201ba80745a63fe9f4
Nurperi <nurperikasi@gmail.com>
2021-03-24 22:09:14 +0300

    2.1.3

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Dog;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,11 +11,14 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
+
+        AnimalsCage dog = applicationContext.getBean(AnimalsCage.class);
+        dog.whatAnimalSay();
     }
 
 }
